5480b06cdf19c44d1d3fc768b44a706bb314ebb0
Prosper Atu <atu.prosper@gmail.com>
2022-12-01 01:49:06 +0100

    Structured the album struct

--- main.go
+++ main.go
@@ -8,11 +8,10 @@ import (
 
 // album represents data about a record album.
 type album struct {
-	ID        string  `json:"id"`
-	Title     string  `json:"title"`
-	Artist    string  `json:"artist"`
-	Price     float64 `json:"price"`
-	Published int
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 // albums slice (Array) to seed record album data.
